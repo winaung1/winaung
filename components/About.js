4af107e6f1130942d0a32590be4f8ffb8f4c1529
Win Aung <91712429+winaung1@users.noreply.github.com>
2023-07-04 01:33:01 -0600

    updated port

--- components/About.js
+++ components/About.js
@@ -6,9 +6,11 @@ function About() {
       <h1 className='pt-10 text-2xl pb-2 text-white lg:text-3xl'>About</h1>
       <div className='flex space-x-2 lg:w-[880px] mx-auto'>
       <div className='w-[72px] h-[1px] bg-[#EBB943] mt-2 lg:w-32'/>
-      <p className='text-sm lg:text-2xl'>I’m a software engineer who loves creating websites and apps. 
-      I believe that you have to be able to see the big picture when you’re building something, and that’s
-       why I like to work toward the same goal—to make your site better than you could ever do on your own.</p>
+      <p className='text-sm lg:text-2xl'>Web Developer with a strong command of HTML, CSS, JavaScript, 
+      and front-end frameworks. Skilled in creating responsive and visually appealing websites. 
+      Experienced in back-end technologies and 
+      integrating APIs. Passionate about delivering high-quality work
+       and staying updated with industry trends. Effective problem solver and team collaborator.</p>
       </div>
     </div>
   )
--- components/Experience.js
+++ components/Experience.js
@@ -8,14 +8,14 @@ function Experience() {
         <div className='bg-white drop-shadow-2xl h-6 px-1 rounded-r-full'>2021</div>
         <div className='flex item-center justify-center flex-col pt-1'>
           <div className='w-3 h-3 bg-[#EBB943] glow rounded-full mb-2'></div>
-          <div className='bg-white w-[1px] h-60 sm:h-40 md:h-32 mx-auto'></div>
+          <div className='bg-white w-[1px] h-60 sm:h-40 md:h-40 mx-auto'></div>
         </div>
         <div>
           <h1 className='text-white lg:text-2xl'>Freelance Web Developer</h1>
           <p className='pb-2'>USA, Utah</p>
           <p className='max-w-3xl lg:text-xl'>
-            My main work of line is doing website for clients from scratch using all the technologies in the skills section.
-            I enjoy this work because I can come up with different ideas and be creative.
+          Obtained web development experience through freelance work, delivering responsive and visually appealing websites while collaborating closely with clients. 
+          Developed strong problem-solving skills and met project deadlines effectively. and enhanced my ability to work independently while meeting project deadlines.
           </p>
         </div>
       </div>
@@ -23,15 +23,13 @@ function Experience() {
         <div className='bg-white drop-shadow-2xl h-6 px-1 rounded-r-full'>2021</div>
         <div className='flex item-center justify-center flex-col pt-1'>
           <div className='w-3 h-3 bg-[#EBB943] glow rounded-full mb-2'></div>
-          <div className='bg-white w-[1px] h-60 sm:h-40 md:h-32 mx-auto'></div>
+          <div className='bg-white w-[1px] h-40 sm:h-40 md:h-32 mx-auto'></div>
         </div>
         <div>
           <h1 className='text-white lg:text-2xl'>Software Engineer</h1>
           <p className='pb-2'>USA, Utah</p>
           <p className='max-w-3xl lg:text-xl'>
-            I had to fetch data using backend Api and display it in the front page.
-            I was working for Vanderhall Motor Works, a small startup company. I provided alot of value during my time there.
-            I enjoyed working there.
+          Fetched and displayed data using backend API for Vanderhall Motor Works, a startup. Provided significant value and enjoyed my time there.
           </p>
         </div>
       </div>
